7d2ab53049c382397c18661fba59ffcb210faf25
RAFIKHA  SHAHA <rafikshah2272@gmail.com>
2023-09-13 14:26:26 +0530

    Create list_asc_desc.py

---
+++ list_asc_desc.py
@@ -0,0 +1,18 @@
+# cook your dish here
+flag=0
+t=int(input())
+for i in range(t):
+    n1=int(input())
+    lst=[]
+    for i in range(n1):
+        n2=input()
+        lst.append(n2)
+    for i in range(len(lst)-1):
+        if lst[i]<lst[i+1]:
+            flag=1
+        else:
+            flag=0
+    if flag==1:
+        print("list in the form of ascendeing order")
+    else:
+        print("in desc")   
